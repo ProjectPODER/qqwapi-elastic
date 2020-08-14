2d746c833e15df2488e675f0f8c994e420750d1c
Martín S <martin@rindecuentas.org>
2020-08-13 19:31:00 -0500

    Implemented all parameters

--- controllers/libv3.js
+++ controllers/libv3.js
@@ -40,6 +40,90 @@ const query_definitions = {
     type: "range-lt",
     field: "compiledRelease.contract_amount.supplier"
   },
+
+
+  // apiFilterName: {
+  //   min: "contract_count_supplier_min",
+  //   max: "contract_count_supplier_max",
+  // },
+  // apiFieldNames:["compiledRelease.contract_count.supplier"],
+  "contract_count_supplier_min": {
+    context: "query",
+    type: "range-gt",
+    field: "compiledRelease.contract_count.supplier"
+  },
+  "contract_count_supplier_max": {
+    context: "query",
+    type: "range-lt",
+    field: "compiledRelease.contract_count.supplier"
+  },
+
+  // apiFilterName: {
+  //   min: "contract_amount_buyer_min",
+  //   max: "contract_amount_buyer_max",
+  // },    
+  // apiFieldNames:["compiledRelease.contract_amount.buyer"],
+  "contract_amount_buyer_min": {
+    context: "query",
+    type: "range-gt",
+    field: "compiledRelease.contract_amount.buyer"
+  },
+  "contract_amount_buyer_max": {
+    context: "query",
+    type: "range-lt",
+    field: "compiledRelease.contract_amount.buyer"
+  },
+
+  // apiFilterName: {
+  //   min: "contract_count_buyer_min",
+  //   max: "contract_count_buyer_max",
+  // },    
+  // apiFieldNames:["compiledRelease.contract_count.buyer"],
+  "contract_count_buyer_min": {
+    context: "query",
+    type: "range-gt",
+    field: "compiledRelease.contract_count.buyer"
+  },
+  "contract_count_buyer_max": {
+    context: "query",
+    type: "range-lt",
+    field: "compiledRelease.contract_count.buyer"
+  },
+
+
+  // apiFilterName: {
+  //   min: "start_date_min",
+  //   max: "start_date_max"
+  // },
+  // apiFieldNames:["compiledRelease.contracts.period.startDate"],
+  "start_date_min": {
+    context: "query",
+    type: "range-gt",
+    field: "compiledRelease.contracts.period.startDate"
+  },
+  "start_date_max": {
+    context: "query",
+    type: "range-lt",
+    field: "compiledRelease.contracts.period.startDate"
+  },
+
+
+  // apiFilterName: {
+  //   min: "amount_min",
+  //   max: "amount_max"
+  // },    
+  // apiFieldNames:["compiledRelease.total_amount"],
+  "amount_min": {
+    context: "query",
+    type: "range-gt",
+    field: "compiledRelease.total_amount"
+  },
+  "amount_max": {
+    context: "query",
+    type: "range-lt",
+    field: "compiledRelease.total_amount"
+  },
+
   // apiFilterName: "classification",
   // apiFieldNames:["compiledRelease.classification"],
   "classification": {
@@ -47,6 +131,117 @@ const query_definitions = {
     type: "match_phrase",
     field: "compiledRelease.classification"
   },
+  // apiFilterName: "title",
+  // apiFieldNames:["compiledRelease.contracts.title"],
+  "title": {
+    context: "must",
+    type: "match",
+    field: "compiledRelease.contracts.title"
+  },
+
+
+  // apiFilterName: "supplier_name",
+  // apiFieldNames:["compiledRelease.awards.suppliers.name"],
+  "supplier_name": {
+    context: "must",
+    type: "match",
+    field: "compiledRelease.awards.suppliers.name"
+  },
+
+  // apiFilterName: "buyer_name",
+  // apiFieldNames:["compiledRelease.parties.memberOf.name"],
+  "buyer_name": {
+    context: "must",
+    type: "match",
+    field: "compiledRelease.parties.memberOf.name"
+  },
+
+  // apiFilterName: "funder_name",
+  // apiFieldNames:["funder"],
+  // TODO: Match party type too
+  "funder_name": {
+    context: "must",
+    type: "match",
+    field: "compiledRelease.parties.name"
+  },
+
+  // apiFilterName: "subclassification",
+  // apiFieldNames:["compiledRelease.subclassification"],
+  "subclassification": {
+    context: "query",
+    type: "match_phrase",
+    field: "compiledRelease.subclassification"
+  },
+  // apiFilterName: "id",
+  // apiFieldNames:["compiledRelease.contracts.id"],
+  "id": {
+    context: "query",
+    type: "match_phrase",
+    field: "compiledRelease.contracts.id"
+  },
+
+  // apiFilterName: "country",
+  // apiFieldNames:["compiledRelease.area.id"],
+  "country": {
+    context: "query",
+    type: "match_phrase",
+    field: "compiledRelease.area.id"
+  },
+  // apiFilterName: "buyer_id",
+  // apiFieldNames:["compiledRelease.buyer.id"],
+  "buyer_id": {
+    context: "query",
+    type: "match_phrase",
+    field: "compiledRelease.buyer.id"
+  },
+  // apiFilterName: "contact_point_name",
+  // apiFieldNames:["compiledRelease.parties.contactPoint.name"],
+  "contact_point_name": {
+    context: "query",
+    type: "match_phrase",
+    field: "compiledRelease.parties.contactPoint.name"
+  },
+
+  // apiFilterName: "procurement_method",
+  // apiFieldNames:["compiledRelease.tender.procurementMethod"],
+  "procurement_method": {
+    context: "query",
+    type: "match_phrase",
+    field: "compiledRelease.tender.procurementMethod"
+  },
+  // apiFilterName: "source",
+  // apiFieldNames: ["compiledRelease.source.id"],
+  "source": {
+    context: "query",
+    type: "match_phrase",
+    field: "compiledRelease.source.id"
+  },
+
+  // OK apiFilterName: "sort",
+  // apiFieldNames:["sort"],
+  "sort": {
+    context: "body",
+    type: "sort",
+    field: "sort"
+  },
+
+  // OK apiFilterName: "sort_direction",
+  // apiFieldNames:["_sortDirection"],
+  "sort_direction": {
+    context: "body",
+    type: "direction",
+    field: "sort"
+  },
+
+
+  // OK apiFilterName: "offset",
+  // apiFieldNames:["offset"],
+  "offset": {
+    context: "body",
+    field: "from"
+  },
+
+  //OK 
   "limit": {
     context: "body",
     field: "size"
@@ -54,86 +249,8 @@ const query_definitions = {
 }
 
 
-
-
-// apiFilterName: {
-//   min: "contract_count_supplier_min",
-//   max: "contract_count_supplier_max",
-// },
-// apiFieldNames:["compiledRelease.contract_count.supplier"],
-
-// apiFilterName: {
-//   min: "contract_amount_buyer_min",
-//   max: "contract_amount_buyer_max",
-// },    
-// apiFieldNames:["compiledRelease.contract_amount.buyer"],
-
-// apiFilterName: {
-//   min: "contract_count_buyer_min",
-//   max: "contract_count_buyer_max",
-// },    
-// apiFieldNames:["compiledRelease.contract_count.buyer"],
-
-
-// apiFilterName: "subclassification",
-// apiFieldNames:["compiledRelease.subclassification"],
-
-
-// apiFilterName: "title",
-// apiFieldNames:["compiledRelease.contracts.title"],
-
-// apiFilterName: "id",
-// apiFieldNames:["compiledRelease.contracts.id"],
-
-// apiFilterName: "supplier_name",
-// apiFieldNames:["compiledRelease.awards.suppliers.name"],
-
-// apiFilterName: "buyer_name",
-// apiFieldNames:["compiledRelease.parties.memberOf.name"],
-
-// apiFilterName: "funder_name",
-// apiFieldNames:["funder"],
-
-// apiFilterName: "country",
-// apiFieldNames:["compiledRelease.area.id"],
-
-// apiFilterName: "buyer_id",
-// apiFieldNames:["compiledRelease.buyer.id"],
-
-// apiFilterName: "contact_point_name",
-// apiFieldNames:["compiledRelease.parties.contactPoint.name"],
-
-// apiFilterName: {
-//   min: "start_date_min",
-//   max: "start_date_max"
-// },
-// apiFieldNames:["compiledRelease.contracts.period.startDate"],
-
-// apiFilterName: {
-//   min: "amount_min",
-//   max: "amount_max"
-// },    
-// apiFieldNames:["compiledRelease.total_amount"],
-
-// apiFilterName: "procurement_method",
-// apiFieldNames:["compiledRelease.tender.procurementMethod"],
-
-// apiFilterName: "source",
-// apiFieldNames: ["compiledRelease.source.id"],
-
-// apiFilterName: "sort",
-// apiFieldNames:["sort"],
-
-// apiFilterName: "sort_direction",
-// apiFieldNames:["_sortDirection"],
-
-// apiFilterName: "offset",
-// apiFieldNames:["offset"],
-
-
-
 function paramsToBody(params) {
-  const body={}; 
+  const body={ sort: []}; 
   Object.keys(params).forEach( param => { 
     if (params[param]) {
       let qdp = query_definitions[param];
@@ -150,6 +267,20 @@ function paramsToBody(params) {
               body.query.bool.filter=[]
             }
             body.query.bool.filter.push({term: { [qdp.field]: params[param]}}); 
+          }
+        }
+        if (qdp.context == "must") {
+          if (qdp.type == "match") {
+            if (!body.query) {
+              body.query={}
+            }
+            if (!body.query.bool) {
+              body.query.bool={}
+            }
+            if (!body.query.bool.must) {
+              body.query.bool.must=[]
+            }
+            body.query.bool.must.push({match: { [qdp.field]: params[param]}}); 
           }
         }
         if (qdp.context == "query") {
@@ -179,7 +310,22 @@ function paramsToBody(params) {
           }
         }
         if (qdp.context == "body") {
-          body[qdp.field] = params[param]; 
+          if (qdp.type=="sort") {
+            if (!body.sort) {
+              body.sort = [];
+            }
+            body.sort.push({[params[param]]: ""});             
+          }
+          if (qdp.type=="direction") {
+            if (!body.sort) {
+              body.sort = [];
+            }
+            body.sort[0][params["sort"]] = {order: params[param]};             
+          }
+          if (!qdp.type) {
+            body[qdp.field] = params[param]; 
+          }
+
         }
   
       }
@@ -209,10 +355,6 @@ async function search (index,params) {
 
   console.log("search searchDocument body",JSON.stringify(searchDocument.body));
 
-  // here we are forcing an index refresh, otherwise we will not
-  // get any result in the consequent search
-  // await client.indices.refresh({ index: 'game-of-thrones' })
-
   // Let's search!
   try {
 
@@ -223,7 +365,6 @@ async function search (index,params) {
   catch(e) {
     return {error: e}
   }
-//   return {}
   // console.log(body.hits.hits[0])
 }
 
