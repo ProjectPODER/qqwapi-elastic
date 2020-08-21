ca70d9e74ecea0e289dddb3759709096bd483348
Martín S <martin@rindecuentas.org>
2020-08-20 22:59:22 -0500

    Fix contracts collection name

--- controllers/contracts.js
+++ controllers/contracts.js
@@ -2,7 +2,7 @@ const prepareOutput = require('./libv3').prepareOutput;
 const search = require('./libv3').search;
 
 function allContracts(context) {;
-  return search("imss",context.params.query).then(results => { return prepareOutput(results) } )
+  return search("contracts",context.params.query).then(results => { return prepareOutput(results) } )
 }
 
 module.exports = {allContracts}
