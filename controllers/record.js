65a972b93ffab6322cb5300f6955984b83cae612
Martín S <martin@rindecuentas.org>
2020-08-25 15:33:38 -0500

    Workig records controller #8

--- controllers/record.js
+++ controllers/record.js
@@ -1,5 +1,7 @@
 const monk = require('monk');
 const clone = require('lodash/clone');
+const lib = require('./libv3');
+
 const dbConnect = process.env.MONGODB_URI || 'localhost:27017/da39a3ee5';
 const db = monk(dbConnect);
 
@@ -11,26 +13,12 @@ db.catch(err => {
 
   //We should exit if no connection is made. I'm excluding production to test this.
   if (process.env.NODE_ENV !== "production") {
-    // process.exit();
+    process.exit();
   }
 })
 
 const collection = db.get('records', { castIds: false });
 
-// function allRecords(req, res) {
-//   const query = getQuery(req);
-//   const offset = query.options.skip || 0;
-//   const resultsP = collection.find(query.criteria, query.options);
-//   const countP = collection.count(query);
-
-//   return Promise.all([countP, resultsP])
-//     .then(array => (dataReturn(res, array, offset, null, null)));
-// }
-
-
-// module.exports = {
-//   allRecords,
-// };
 const recordPackageBase = {
   uri: '',
   version: '1.1',
@@ -57,124 +45,34 @@ function addRecordPackage(object, debug) {
   if (debug) {
     console.log("addRecordPackage", object);
   }
-  if (typeof object[1][0] == "object") {
+  if (typeof object[0] == "object") {
     const recordPackage = clone(recordPackageBase);
 
-    recordPackage.records = object[1];
-    recordPackage.uri = `https://api.beta.quienesquien.wiki/v2/contracts/${object[1][0].ocid}`;
-    recordPackage.publishedDate = object[1][0].compiledRelease.date;
-    object[1] = [recordPackage];
-    object[2] = object[1][0].records.length;
+    recordPackage.records = object[0];
+    recordPackage.uri = `https://api.beta.quienesquien.wiki/v2/contracts/${object[0].ocid}`;
+    recordPackage.publishedDate = object[0].compiledRelease.date;
+    object = [recordPackage];
   }
   return object;
 }
 
-
-function allDocuments(query, collection, JOINS, debug=false) {
-  // console.log("q",query.criteria);
-  if (debug) {
-    debug = true;
-    // delete query.criteria.debug;
-    console.log("DEBUG allDocuments",collection.name);
-    console.log("DEBUG allDocuments query 1",JSON.stringify(query,null,4));
-  }
-  const maxTime = 1000*30;
-
-  // query.options.maxTimeMS = maxTime;
-  const countP = collection.count(query.criteria,{maxTimeMS: maxTime}).catch(err => {
-    console.error("allDocuments count error",err);
-    if (err) {
-      return `error: ${err}`;
-    }
-    return err;
-  });
-
-  if (query.embed) {
-    const p = queryToPipeline(query, clone(JOINS));
-    const pipeline = arrayResultsOptions(query, p);
-
-    const pipelineOptions = {
-    //   allowDiskUse: true,
-      maxTimeMS: maxTime
-    }
-
-    if (debug) {
-      console.log("DEBUG allDocuments query 2",JSON.stringify(query,null,4));
-      console.log("DEBUG allDocuments pipeline",JSON.stringify(pipeline,null,4));
-    }
-
-    const resultsP = collection.aggregate(pipeline, pipelineOptions);
-
-    return Promise.all([countP, resultsP]);
-  }
-
-  if (debug) {
-    console.log("DEBUG allDocuments query",JSON.stringify(query,null,4));
-  }
-
-  const resultsP = collection.find(query.criteria, query.options).catch(err => {
-    console.error("allDocuments error",err);
-    if (err) {
-      return `error: ${err}`;
-    }
-    return err;
-  });
-
-  return Promise.all([countP, resultsP]);
-}
-
-
-
-function dataReturn(res, array, offset, limit, embed, objectFormat, debug) {
-  let data = array[1];
-  let status = "success";
-  let size = 0;
-
-  // Contracts have a different structure and their length comes in the third item in the array
-  // console.log("dataReturn",array);
-  if (array[2] || (array[1] && typeof array[1] == "object")) {
-    size = array[2] || array[1].length;
-    if (embed) {
-      data = array[1].map(o => (objectFormat(o)));
-    }
-  }
-  else {
-    console.error("dataReturn error",array);
-    status = "error";
-  }
-
-  if (debug) {
-    console.log("dataReturn",size);
-  }
-
-  const pageSize = limit || size;
-  const pagesNum = Math.ceil((array[0] / pageSize));
-
-
-  res.set('Content-Type', 'application/json; charset=utf-8');
-  res.json({
-    status: status,
-    size,
-    limit,
-    offset,
-    pages: pagesNum,
-    count: array[0],
-    data,
-  });
-}
-
-
 async function allRecords(context) {
-  const debug = context.params.query.debug;
+  const debug = context.req.originalUrl.indexOf("debug") > -1;
   const query = {ocid: context.params.query.id };
-  const offset = 0; //query.options.skip || 0;
-  // console.log("allContracts debug",debug);
+  const offset = 0;
+  // console.log("allContracts debug",debug,context.params.query);
 
   if (debug) {
     console.log("DEBUG allContracts query",JSON.stringify(query,null,4));
   }
 
-  allDocuments(query, collection, [], debug)
+  return collection.find(query, {}).catch(err => {
+    console.error("allDocuments error",err);
+    if (err) {
+      return `error: ${err}`;
+    }
+    return err;
+  })
     .then(array => (addRecordPackage(array, debug)))
     .catch(err => {
       console.error('allContracts query error', err);
@@ -183,14 +81,21 @@ async function allRecords(context) {
       }
       return false;
     })
-    .then(array => {
-      return dataReturn(res, array, offset, query.options.limit, query.embed, contractMapData, debug)
-    } )
-    .catch(err => {
-      console.error("allContracts return error",err);
-      res.json({"error": "error"})
-    });
-
+    .then((array) => {
+      if (debug) {
+        console.log("return",array);
+      }
+      return {
+        status: "success",
+        size:1,
+        limit:1,
+        offset:0,
+        pages: 1,
+        count: 1,
+        count_precission: "eq",
+        data: array,
+    };      
+    })
 }
 
 
