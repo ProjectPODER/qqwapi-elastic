eb327b2fa862326ea6ef0a485f1af9b5594b3b3a
Martín S <martin@rindecuentas.org>
2021-01-28 22:19:16 -0600

    Fix autocomplete

--- controllers/autocomplete.js
+++ controllers/autocomplete.js
@@ -8,7 +8,9 @@ function autocomplete(context) {
     .then(results => { 
       return lib.embed(controllerIndex,context.params,results,debug) 
     })
-    .then(lib.prepareOutput)
+    .then(body => {
+      return lib.prepareOutput(body, context, debug)
+    })
 }
 
 function autocompleteEmptyResponse(context) {
